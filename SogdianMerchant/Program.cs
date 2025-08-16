962f7b1e40a8a65b1f0474d46fd90972ca237deb
tonybierman <tonybierman@gmail.com>
2025-08-16 14:43:22 -0500

    Fluent UI

--- SogdianMerchant/Program.cs
+++ SogdianMerchant/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.FluentUI.AspNetCore.Components;
 using SogdianMerchant;
 using SogdianMerchant.Services;
 
@@ -7,6 +8,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddFluentUIComponents();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IRandomGenerator, RandomGenerator>();
 builder.Services.AddScoped<ICalculationService, CalculationService>();
